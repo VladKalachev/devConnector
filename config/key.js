bc957a05d2f5ff5d264b147e453720bc05483dfa
Corvax <vlvete1990@yandex.ru>
2018-07-29 17:16:32 +0300

    v1.0

--- config/key.js
+++ config/key.js
@@ -1,5 +1,6 @@
-module.exports = {
-    mongoURI:
-    'mongodb+srv://admin:admin@cluster0-ci2ir.mongodb.net/test?retryWrites=true',
-    secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }
+  
